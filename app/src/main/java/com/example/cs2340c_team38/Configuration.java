ccf8f743c4b741dcc615523fb5429a52567db774
Keane Zhang <wristy8@gmail.com>
2023-09-18 14:09:03 -0400

    added initial screen and set up for configuration screen

---
+++ app/src/main/java/com/example/cs2340c_team38/Configuration.java
@@ -0,0 +1,14 @@
+package com.example.cs2340c_team38;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Configuration extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_configuration);
+    }
+}
