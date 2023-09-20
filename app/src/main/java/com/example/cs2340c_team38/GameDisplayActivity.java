11306e2bce44f7c4e4973346cc315721bf73ac22
samreenfarooqui <samreen.farooqui678@gmail.com>
2023-09-20 13:26:29 -0400

    updates

---
+++ app/src/main/java/com/example/cs2340c_team38/GameDisplayActivity.java
@@ -0,0 +1,15 @@
+package com.example.cs2340c_team38;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GameDisplayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_display);
+
+    }
+}
