180a08fe8577e348d1ab1b99c074be2dbd8dea1f
Keane Zhang <wristy8@gmail.com>
2023-09-20 13:22:14 -0400

    implemented logic for config screen, and images for characters

--- app/src/main/java/com/example/cs2340c_team38/MainActivity.java
+++ app/src/main/java/com/example/cs2340c_team38/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.cs2340c_team38;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,13 +14,16 @@ public class MainActivity extends AppCompatActivity { // welcome screen
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         Button buttonStart = (Button) findViewById(R.id.buttonStart);
+
+
         buttonStart.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                // body here
+                Intent intent = new Intent(MainActivity.this, ConfigActivity.class);
+                startActivity(intent);
             }
         });
+
 
         Button buttonExit = (Button) findViewById(R.id.buttonExit);
         buttonExit.setOnClickListener(new View.OnClickListener() {
