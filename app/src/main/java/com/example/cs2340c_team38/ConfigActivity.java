c27f33b95700b57a9279e0108f596fa308206a3f
Keane Zhang <wristy8@gmail.com>
2023-10-05 01:18:10 -0400

    MVVM config screen

--- app/src/main/java/com/example/cs2340c_team38/ConfigActivity.java
+++ app/src/main/java/com/example/cs2340c_team38/ConfigActivity.java
@@ -2,66 +2,107 @@ package com.example.cs2340c_team38;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.TextUtils;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.RadioGroup;
-import android.widget.Spinner;
+import android.util.Log;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.cs2340c_team38.databinding.ActivityConfigBinding;
+import com.example.cs2340c_team38.viewmodels.ConfigViewModel;
 
 public class ConfigActivity extends AppCompatActivity {
-    private EditText editPlayerName;
-    private Spinner spinnerDifficulty;
-    private RadioGroup radioGroupCharacterSprite;
-    private Button buttonContinue;
+    private ConfigViewModel viewModel;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_config);
+        ActivityConfigBinding binding;
+        viewModel = new ViewModelProvider(this).get(ConfigViewModel.class);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_config);
+        binding.setViewModel(viewModel);
 
-        editPlayerName = findViewById(R.id.editPlayerName);
-        spinnerDifficulty = findViewById(R.id.spinnerDifficulty);
-        radioGroupCharacterSprite = findViewById(R.id.radioGroupCharacterSprite);
-        buttonContinue = findViewById(R.id.buttonContinue);
-
-        buttonContinue.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (isValidConfig()) {
-                    Intent intent = new Intent(ConfigActivity.this,
-                            GameDisplayActivity.class);
-                    intent.putExtra("PLAYER_NAME", editPlayerName.getText().toString());
-                    intent.putExtra("DIFFICULTY",
-                            spinnerDifficulty.getSelectedItem().toString());
-                    intent.putExtra("CHARACTER_SPRITE",
-                            radioGroupCharacterSprite.getCheckedRadioButtonId());
-                    startActivity(intent);
-                } else {
-                    Toast.makeText(ConfigActivity.this, "Please fill in all details",
-                            Toast.LENGTH_SHORT).show();
-                }
+        viewModel.getIsValidConfig().observe(this, isValid -> {
+            if (isValid) {
+                Toast.makeText(ConfigActivity.this, ":)", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(ConfigActivity.this, GameDisplayActivity.class);
+                intent.putExtra("PLAYER_NAME", viewModel.getPlayerName().getValue());
+                intent.putExtra("DIFFICULTY", viewModel.getDifficulty().getValue());
+                intent.putExtra("CHARACTER_SPRITE", viewModel.getCharacterSprite().getValue());
+                startActivity(intent);
+            } else {
+                Toast.makeText(ConfigActivity.this, "Please fill in all details", Toast.LENGTH_SHORT).show();
             }
         });
     }
-
-    private boolean isValidConfig() {
-        if (TextUtils.isEmpty(editPlayerName.getText().toString().trim())) {
-            return false;
-        }
-
-        if (spinnerDifficulty.getSelectedItem() == null) {
-            return false;
-        }
-
-        if (radioGroupCharacterSprite.getCheckedRadioButtonId() == -1) {
-            return false;
-        }
-
-        return true;
-    }
 }
+
+// package com.example.cs2340c_team38;
+
+// import android.content.Intent;
+// import android.os.Bundle;
+// import android.text.TextUtils;
+// import android.view.View;
+// import android.widget.Button;
+// import android.widget.EditText;
+// import android.widget.RadioGroup;
+// import android.widget.Spinner;
+// import android.widget.Toast;
+
+// import androidx.appcompat.app.AppCompatActivity;
+
+// public class ConfigActivity extends AppCompatActivity {
+//     private EditText editPlayerName;
+//     private Spinner spinnerDifficulty;
+//     private RadioGroup radioGroupCharacterSprite;
+//     private Button buttonContinue;
+
+//     @Override
+//     protected void onCreate(Bundle savedInstanceState) {
+//         super.onCreate(savedInstanceState);
+//         setContentView(R.layout.activity_config);
+
+//         editPlayerName = findViewById(R.id.editPlayerName);
+//         spinnerDifficulty = findViewById(R.id.spinnerDifficulty);
+//         radioGroupCharacterSprite = findViewById(R.id.radioGroupCharacterSprite);
+//         buttonContinue = findViewById(R.id.buttonContinue);
+
+//         buttonContinue.setOnClickListener(new View.OnClickListener() {
+//             @Override
+//             public void onClick(View view) {
+//                 if (isValidConfig()) {
+//                     Intent intent = new Intent(ConfigActivity.this,
+//                             GameDisplayActivity.class);
+//                     intent.putExtra("PLAYER_NAME", editPlayerName.getText().toString());
+//                     intent.putExtra("DIFFICULTY",
+//                             spinnerDifficulty.getSelectedItem().toString());
+//                     intent.putExtra("CHARACTER_SPRITE",
+//                             radioGroupCharacterSprite.getCheckedRadioButtonId());
+//                     startActivity(intent);
+//                 } else {
+//                     Toast.makeText(ConfigActivity.this, "Please fill in all details",
+//                             Toast.LENGTH_SHORT).show();
+//                 }
+//             }
+//         });
+//     }
+
+//     private boolean isValidConfig() {
+//         if (TextUtils.isEmpty(editPlayerName.getText().toString().trim())) {
+//             return false;
+//         }
+
+//         if (spinnerDifficulty.getSelectedItem() == null) {
+//             return false;
+//         }
+
+//         if (radioGroupCharacterSprite.getCheckedRadioButtonId() == -1) {
+//             return false;
+//         }
+
+//         return true;
+//     }
+// }
 
--- app/src/main/java/com/example/cs2340c_team38/MainActivity.java
+++ app/src/main/java/com/example/cs2340c_team38/MainActivity.java
@@ -17,47 +17,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-      ActivityMainBinding binding;
-      binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
-      viewModel = new ViewModelProvider(this).get(MainViewModel.class);
-      binding.setViewModel(viewModel);
+        ActivityMainBinding binding = DataBindingUtil.setContentView(
+                this, R.layout.activity_main
+        );
+        viewModel = new ViewModelProvider(this).get(MainViewModel.class);
+        binding.setViewModel(viewModel);
 
-      viewModel.getStartEvent().observe(this, message -> {
-            Intent intent = new Intent(MainActivity.this, ConfigActivity.class);
-            startActivity(intent);
-      });
-
-      viewModel.getEndEvent().observe(this, message -> {
-          finish();
-      });
+        viewModel.getStartEvent().observe(this, message -> startActivity(
+                new Intent(MainActivity.this, ConfigActivity.class)
+        ));
+        viewModel.getEndEvent().observe(this, message -> finish());
     }
 }
-
-//public class MainActivity extends AppCompatActivity { // welcome screen
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-//
-//        Button buttonStart = (Button) findViewById(R.id.buttonStart);
-//
-//
-//        buttonStart.setOnClickListener(new View.OnClickListener() {
-//            public void onClick(View v) {
-//                Intent intent = new Intent(MainActivity.this, ConfigActivity.class);
-//                startActivity(intent);
-//            }
-//        });
-//
-//
-//        Button buttonExit = (Button) findViewById(R.id.buttonExit);
-//        buttonExit.setOnClickListener(new View.OnClickListener() {
-//            public void onClick(View v) {
-//                finish();
-//            }
-//        });
-//    }
-//
-//
-//}
