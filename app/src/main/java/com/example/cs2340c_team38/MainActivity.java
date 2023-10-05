61a2f13eb7089ec2ff815e825bf5336979b89e15
Keane Zhang <wristy8@gmail.com>
2023-10-04 23:31:30 -0400

    fixed build.gradle

--- app/src/main/java/com/example/cs2340c_team38/MainActivity.java
+++ app/src/main/java/com/example/cs2340c_team38/MainActivity.java
@@ -1,37 +1,61 @@
 package com.example.cs2340c_team38;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity { // welcome screen
+import com.example.cs2340c_team38.databinding.ActivityMainBinding;
+import com.example.cs2340c_team38.viewmodels.MainViewModel;
+
+public class MainActivity extends AppCompatActivity {
+    private MainViewModel viewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        Button buttonStart = (Button) findViewById(R.id.buttonStart);
+      ActivityMainBinding binding;
+      binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+      viewModel = new ViewModelProvider(this).get(MainViewModel.class);
+      binding.setViewModel(viewModel);
 
-
-        buttonStart.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, ConfigActivity.class);
-                startActivity(intent);
-            }
-        });
-
-
-        Button buttonExit = (Button) findViewById(R.id.buttonExit);
-        buttonExit.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                finish();
-            }
-        });
+      viewModel.getStartEvent().observe(this, message -> {
+            Intent intent = new Intent(MainActivity.this, ConfigActivity.class);
+            startActivity(intent);
+      });
     }
-
-
 }
+
+//public class MainActivity extends AppCompatActivity { // welcome screen
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//
+//        Button buttonStart = (Button) findViewById(R.id.buttonStart);
+//
+//
+//        buttonStart.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View v) {
+//                Intent intent = new Intent(MainActivity.this, ConfigActivity.class);
+//                startActivity(intent);
+//            }
+//        });
+//
+//
+//        Button buttonExit = (Button) findViewById(R.id.buttonExit);
+//        buttonExit.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View v) {
+//                finish();
+//            }
+//        });
+//    }
+//
+//
+//}
--- app/src/main/java/com/example/cs2340c_team38/viewmodels/MainViewModel.java
+++ app/src/main/java/com/example/cs2340c_team38/viewmodels/MainViewModel.java
@@ -3,42 +3,22 @@ package com.example.cs2340c_team38.viewmodels;
 import static androidx.core.content.ContextCompat.startActivity;
 
 import android.content.Context;
-import android.content.Intent;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 import com.example.cs2340c_team38.ConfigActivity;
-
 public class MainViewModel extends ViewModel {
-    private MutableLiveData<String> message = new MutableLiveData<>();
-
-    public LiveData<String> getMessage() {
-        return message;
+    public MutableLiveData<Void> getStartEvent() {
+        return startEvent;
     }
 
-    public void setMessage(String message) {
-        this.message.setValue(message);
-    }
+    private MutableLiveData<Void> startEvent = new MutableLiveData<>();
 
-    public class MainViewModel extends ViewModel {
-        private MutableLiveData<String> message = new MutableLiveData<>();
-
-        public LiveData<String> getMessage() {
-            return message;
-        }
-
-        public void setMessage(String message) {
-            this.message.setValue(message);
-        }
-
-        public void onButtonStartClicked(Context context) {
-            // Handle button start click event
-            message.setValue("Button Start Clicked");
-            Intent intent = new Intent(context, ConfigActivity.class);
-            context.startActivity(intent);
-        }
+    public void onButtonStartClicked() {
+        // Handle button start click event
+        startEvent.setValue(null);
     }
 
     public void onButtonExitClicked() {
