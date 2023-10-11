f534aa782d6c4f2284e0ef8b5fb05f6eee3abaca
Keane Zhang <wristy8@gmail.com>
2023-10-11 12:49:55 -0400

    made hard coded score list

--- app/src/main/java/com/example/cs2340c_team38/views/EndActivity.java
+++ app/src/main/java/com/example/cs2340c_team38/views/EndActivity.java
@@ -17,15 +17,27 @@ public class EndActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_end);
+        populateHardCodedEntries();
+        displayLeaderboard();
     }
 
     private void displayLeaderboard() {
         Leaderboard leaderboard = Leaderboard.getInstance();
         List<Leaderboard.ScoreEntry> topScores = leaderboard.getTopScores(5);
-        ListView l = findViewById(R.id.list);
-
-
-        // TODO: Display topScores in the desired format on the EndActivity screen
-        // You might use an adapter to populate a ListView or RecyclerView.
+        ListView leaderboardListView = findViewById(R.id.list);
+        LeaderboardAdapter adapter = new LeaderboardAdapter(this, topScores);
+        leaderboardListView.setAdapter(adapter);
     }
+
+    private void populateHardCodedEntries() {
+        Leaderboard leaderboard = Leaderboard.getInstance();
+
+        // Adding 5 hard-coded entries
+        leaderboard.addScore("Alice", 5000);
+        leaderboard.addScore("Bob", 4500);
+        leaderboard.addScore("Charlie", 4900);
+        leaderboard.addScore("Dave", 5100);
+        leaderboard.addScore("Eve", 4800);
+    }
+
 }
