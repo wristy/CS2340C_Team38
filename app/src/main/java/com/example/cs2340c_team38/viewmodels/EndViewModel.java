78b9c2414feae5be2e7ffffbfb4836f314b19c30
Keane Zhang <wristy8@gmail.com>
2023-10-12 17:59:08 -0400

    added restart button and fixed exit button

---
+++ app/src/main/java/com/example/cs2340c_team38/viewmodels/EndViewModel.java
@@ -0,0 +1,18 @@
+package com.example.cs2340c_team38.viewmodels;
+
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class EndViewModel extends ViewModel {
+
+    private MutableLiveData<Void> endEvent = new MutableLiveData<>();
+
+    public MutableLiveData<Void> getEndEvent() {
+        return endEvent;
+    }
+
+    public void onButtonRestartClicked() {
+        endEvent.setValue(null);
+    }
+
+}
