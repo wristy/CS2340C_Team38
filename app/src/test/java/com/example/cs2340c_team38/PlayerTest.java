e94d0d7dda8cf49009ecc703e1a5cb3fc04792c3
patrisiyarum <prumyantseva3@gatech.edu>
2023-10-12 17:38:52 -0400

    tests made for config model

---
+++ app/src/test/java/com/example/cs2340c_team38/PlayerTest.java
@@ -0,0 +1,42 @@
+package com.example.cs2340c_team38;
+import com.example.cs2340c_team38.model.Player;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+public class PlayerTest {
+
+    private Player player;
+
+    @Before
+    public void setUp() {
+        player = Player.getPlayer();
+    }
+    @Test
+    public void testDefaultValues() {
+        assertEquals(0.0, player.getX(), 0.001);
+        assertEquals(0.0, player.getY(), 0.001);
+        assertEquals(5.0, player.getMovementSpeed(), 0.001);
+    }
+
+    @Test
+    public void testMoveUp() {
+        player.setY(10.0);
+        player.moveUp();
+        assertEquals(5.0, player.getY(), 0.001);
+    }
+
+    @Test
+    public void testMoveDown() {
+        player.setY(10.0);
+        player.moveDown();
+        assertEquals(15.0, player.getY(), 0.001);
+    }
+
+    @Test
+    public void testMoveLeft() {
+        player.setX(10.0);
+        player.moveLeft();
+        assertEquals(5.0, player.getX(), 0.001);
+    }
+}
