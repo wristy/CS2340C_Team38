e33cc87b35a15aee030b0726429d33038b2c3ab8
ariasharma1 <ariasharmagt@gmail.com>
2023-11-12 20:04:11 -0500

    added alien enemy tests

--- app/src/test/java/com/example/cs2340c_team38/AlienEnemyTest.java
+++ app/src/test/java/com/example/cs2340c_team38/AlienEnemyTest.java
@@ -1,45 +1,57 @@
-/*package com.example.cs2340c_team38;
+package com.example.cs2340c_team38;
 
 import static org.junit.Assert.assertEquals;
 
 import com.example.cs2340c_team38.model.AlienEnemy;
 import com.example.cs2340c_team38.model.Player;
+
+import org.junit.Before;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.Test;
 
 public class AlienEnemyTest {
-    private class TestablePlayer extends Player {
-        boolean healthReduced = false;
-
-        @Override
-        public void reduceHealth() {
-            super.reduceHealth();
-            healthReduced = true;
-        }
-
-        public boolean isHealthReduced() {
-            return healthReduced;
-        }
-
-        public void resetHealthReduced() {
-            healthReduced = false;
-        }
+private AlienEnemy alienEnemy;
+private Player testPlayer;
+@Before
+    public void setUp() {
+    testPlayer = Player.getPlayer();
+    testPlayer.setHealth(10); // Set a known health value
+    testPlayer.setDamage(1); // Set a known damage value
+    testPlayer.setPosition(3, 3);
+    alienEnemy = new AlienEnemy();
+    alienEnemy.setPlayer(testPlayer);
+}
+@Test
+public void testOnCollisionWithPlayerSamePosition() {
+    testPlayer.setPosition(5, 10);
+    alienEnemy.setPosition(5, 10, null);
+    int initialHealth = testPlayer.getHealth();
+    alienEnemy.onCollisionWithPlayer();
+    assertEquals(initialHealth - 1, testPlayer.getHealth());
+}
+@Test
+public void testOnCollisionWithPlayerDifferentPosition() {
+    testPlayer.setPosition(3, 7);
+    alienEnemy.setPosition(5, 10, null);
+    int initialHealth = testPlayer.getHealth();
+    alienEnemy.onCollisionWithPlayer();
+    assertEquals(initialHealth, testPlayer.getHealth());
+}
+@Test
+public void testUpdateWithCollision() {
+    testPlayer.setPosition(5, 10);
+    alienEnemy.setPosition(5, 10, null);
+    int initialHealth = testPlayer.getHealth();
+    alienEnemy.update(testPlayer, "PlayerMoved", 5, 10);
+    assertEquals(initialHealth - 1, testPlayer.getHealth());
+}
+@Test
+public void testUpdateWithoutCollision() {
+    testPlayer.setPosition(3, 7);
+    alienEnemy.setPosition(5, 10, null);
+    int initialHealth = testPlayer.getHealth();
+    alienEnemy.update(testPlayer, "PlayerMoved", 3, 7);
+    assertEquals(initialHealth, testPlayer.getHealth());
     }
-    private AlienEnemy alienEnemy;
-    private Player testPlayer;
-    @BeforeEach
-    void setUp() {
-        testPlayer = new Player();
-        alienEnemy = new AlienEnemy();
-        alienEnemy.setPlayer(testPlayer);
-    }
-    @Test
-    void testUpdateWithCollision() {
-        testPlayer.setX(10);
-        testPlayer.setY(10);
-        alienEnemy.setPosition(10, 10, null);
-        alienEnemy.update(testPlayer, "", 10, 10);
-        assertEquals(true, testPlayer.isHealthReduced(), "Health should be reduced on update with collision");
-    }
-}*/
+}
 
