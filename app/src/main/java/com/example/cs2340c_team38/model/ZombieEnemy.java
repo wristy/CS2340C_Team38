0ad6ae1de21cd8569a2340d2c45474ddb69c028a
ariasharma1 <ariasharmagt@gmail.com>
2023-11-23 18:14:03 -0500

    modified setPosition method

--- app/src/main/java/com/example/cs2340c_team38/model/ZombieEnemy.java
+++ app/src/main/java/com/example/cs2340c_team38/model/ZombieEnemy.java
@@ -27,7 +27,14 @@ public class ZombieEnemy implements Enemy {
 
     }
     public void setPosition(int x, int y, TileType[][] tileMap) {
-
+        boolean isValid = false;
+        if (x >= 0 && x < tileMap.length && y >=0 && y < tileMap[0].length && tileMap[x][y] == TileType.FLOOR){
+            isValid = true;
+        }
+        if (isValid) {
+            this.x = x;
+            this.y = y;
+        }
     }
     public Player player() {
         return player;
