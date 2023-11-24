ed5cedc18139b45940f5f203e89a2706ad89cdd4
ariasharma1 <ariasharmagt@gmail.com>
2023-11-24 14:21:11 -0500

    deleted private inner TestObserver class

--- app/src/main/java/com/example/cs2340c_team38/model/Observer.java
+++ app/src/main/java/com/example/cs2340c_team38/model/Observer.java
@@ -4,18 +4,4 @@ public interface Observer {
     void update(Observable o, String observable, int x, int y);
 }
 
-/*private class TestObserver implements Observer {
-    private int updatedX;
-    private int updatedY;
-    @Override
-    public void update (int x, int y) {
-        updatedX = x;
-        updatedY = y;
-    }
-    public int getUpdatedX() {
-        return updatedX;
-    }
-    public int getUpdatedY() {
-        return updatedY;
-    }*/
 
